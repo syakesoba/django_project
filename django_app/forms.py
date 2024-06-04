c5a02d24a58e1ac2f2295006fc1d15b313e2dfeb
syakesoba <soccer.akira6@icloud.com>
2024-06-04 22:40:24 +0900

    Implementation of forms.py.

---
+++ django_app/forms.py
@@ -0,0 +1,10 @@
+# Add File 2024/06/04
+from django.forms import ModelForm
+
+from django_app.models import Book
+
+
+class BookForm(ModelForm):
+    class Meta:
+        model = Book
+        fields = ('title')
